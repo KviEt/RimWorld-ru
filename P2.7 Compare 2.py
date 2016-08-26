d2c83b9570458d9d4093e74c72dd65c74931483c
KviEt <KviEt90@gmail.com>
2016-08-26 23:02:24 +0300

    Изменен скрипт.
    Теперь скрипт только для сравнения с исходными defs, пропускает если это
    абстрактный класс или производный класс.

--- P2.7 Compare 2.py
+++ P2.7 Compare 2.py
@@ -20,19 +20,18 @@ tagText = ["label", "rulesStrings", "description", "gerund", "verb", "deathMessa
           ]
 allText = {}
 allTextValue = {}
-pathsOfNewXML = {}
-pathsOfXML = {}
+allTextExcess = {}
 
-def addElement(fileXML, elementText, text):
+def addElement(fileXML, elementText, text, nameDef):
     if("subSounds" not in elementText):
-        if(fileXML not in allText):
-            allText.update([(fileXML, [])])
-        elementsList = allText[fileXML]
+        if(nameDef not in allText):
+            allText[nameDef] = []
+        elementsList = allText[nameDef]
         if(elementText not in elementsList):
             elementsList.append(elementText[:])
-            allTextValue.update([(elementText, text)])
+            allTextValue[elementText] = (text, fileXML)
 
-def findText(element, tagPath, li, fileXML):
+def findText(element, tagPath, li, fileXML, nameDef, necessary):
     for subElement in element:
         if(subElement.tag in tagText):
             countSubElement = list(subElement)
@@ -41,21 +40,21 @@ def findText(element, tagPath, li, fileXML):
                 tagPathSub = tagPath + "." + subElement.tag
                 for sub2Element in subElement:
                     text = sub2Element.text
-                    if(text):
-                        addElement(fileXML, tagPathSub + u"." + str(i), text)
+                    if(text and necessary):
+                        addElement(fileXML, tagPathSub + u"." + str(i), text, nameDef)
                     i = i + 1
             else:
                 text = subElement.text
-                if(text):            
-                    addElement(fileXML, tagPath + u"." + subElement.tag, text)
+                if(text and necessary):            
+                    addElement(fileXML, tagPath + u"." + subElement.tag, text, nameDef)
         else:
             listElement = list(subElement)
             isli = subElement.tag == "li"
             if(listElement):
                 if(isli):
-                    findText(subElement, (tagPath + u"." + str(li))[:], 0, fileXML)
+                    findText(subElement, (tagPath + u"." + str(li))[:], 0, fileXML, nameDef, necessary)
                 else:
-                    findText(subElement, (tagPath + u"." + subElement.tag)[:], 0, fileXML)
+                    findText(subElement, (tagPath + u"." + subElement.tag)[:], 0, fileXML, nameDef, necessary)
             if(isli):
                 li = li + 1
             text = subElement.text
@@ -66,55 +65,69 @@ def findText(element, tagPath, li, fileXML):
                     print tagPath + u"." + subElement.tag + u" значение:"
                     print text
 
+def findDef(root, fileXML):
+    for element in root:
+        name = element.tag
+        if(name[-3:] == u"Def" and name != u"SongDef"):
+            abstract = element.get("Abstract")
+            if(abstract):
+                classAbs = element.get("Name")
+                print u"Внимание! Абстрактный класс %s\n"%classAbs
+                if(classAbs):
+                    tagPath = classAbs
+                else:
+                    tagPath = "None"
+                findText(element, tagPath, 0, fileXML, name, False)
+            else:
+                defName = element.find("defName")
+                if(defName is None):
+                    defName = element.find("DefName")
+                if(defName is not None):
+                    tagPath = defName.text
+                    parent = element.get("ParentName")
+                    if(parent):
+                        findText(element, tagPath, 0, fileXML, name, False)
+                    else:
+                        findText(element, tagPath, 0, fileXML, name, True)
+                else:
+                    print u"Внимание! Неожидаемый элемент %s в файле %s"%(element.tag, fileXML)
+        else:
+            findDef(element, fileXML)
+        
+
 def gather(path, fileXML):
     tree = ET.parse(path)
     root = tree.getroot()
-    for element in root:
-        defName = element.find("defName")
-        if(defName is None):
-            defName = element.find("DefName")
-        if(defName is not None):
-            tagPath = defName.text
-            findText(element, tagPath, 0, fileXML)
-        else:
-            classAbs = element.get("Name")
-            print u"Внимание! Абстрактный класс %s\n"%classAbs
-            if(classAbs):
-                tagPath = classAbs
-            else:
-                tagPath = "None"
-            findText(element, tagPath, 0, fileXML)
+    findDef(root, fileXML)
     
-def compare(path, listElementsText):
+def compare(path, nameDef, fileXML):
     tree = ET.parse(path)
     root = tree.getroot()
     for element in root:
-        if(element.tag in listElementsText):
-            listElementsText.remove(element.tag)
+        nameElement = element.tag
+        if(nameElement in allText[nameDef]):
+            allText[nameDef].remove(nameElement)
+        else:
+            if(fileXML not in allTextExcess):
+                allTextExcess[fileXML] = []
+            allTextExcess[fileXML].append(nameElement)
     
 def findXMLORG(path):
     listOfDir = os.listdir(path)
     for fileXML in listOfDir:
         if ".xml" in fileXML:
             (head, tail) = os.path.split(path)
-            pathNewXML = os.path.join(pathRusOrg, tail)
-            pathsOfNewXML[fileXML] = os.path.join(pathNewXML, fileXML)
             gather(os.path.join(path, fileXML), fileXML)
         else:
             subDir = os.path.join(path, fileXML)
             if os.path.isdir(subDir):
                 findXMLORG(subDir)
 
-def findXMLRUS(path):
+def findXMLRUS(path, nameDef):
     listOfDir = os.listdir(path)
     for fileXML in listOfDir:
         if ".xml" in fileXML:
-            if fileXML in allText:
-                pathXML = os.path.join(path, fileXML)
-                pathsOfXML[fileXML] = pathXML
-                compare(os.path.join(path, fileXML), allText[fileXML])
-            else:
-                print u"Отсуствует файл %s в исходных defs"%fileXML                
+            compare(os.path.join(path, fileXML), nameDef, fileXML)          
 
 def proofFile(path):
     tree = ET.parse(path)
@@ -137,44 +150,27 @@ for element in listOfDir:
     if os.path.isdir(sub): 
         findXMLORG(sub)
 
+
 listOfDir = os.listdir(pathRus)
-for element in listOfDir:
-    sub = os.path.join(pathRus, element)
-    if os.path.isdir(sub): 
-        findXMLRUS(sub)
-
-for key in allText.keys():
-    if key not in pathsOfXML:
-        print u"\nОтсуствует файл для перевода %s\n"%key
-    allElement = allText[key]
-    if(allElement):
-        print u"\nВ файле %s отсутствует:"%key
-        for element in allElement:
-            print u"%s значение: %s"%(element, allTextValue[element])
+for nameDef in listOfDir:
+    if(nameDef in allText):
+        sub = os.path.join(pathRus, nameDef)
+        if os.path.isdir(sub): 
+            findXMLRUS(sub, nameDef)
+    else:
+        print u"Ошибка! Неожидаемый класс %s в переводе"%nameDef
 
 for key in allText.keys():
     allElement = allText[key]
     if(allElement):
-        if(key in pathsOfXML):
-            #parse must use encode utf-8
-            tree = ET.parse(pathsOfXML[key])
-            root = tree.getroot()
-        else:
-            tree = ET.ElementTree()
-            newRoot = ET.Element("LanguageData")
-            tree._setroot(newRoot)
-            root = tree.getroot()
-        if(key in pathsOfNewXML):
-            path = pathsOfNewXML[key]
-            (head, tail) = os.path.split(path)
-            if(not os.path.exists(head)):
-                os.makedirs(head)
-        else:
-            path = os.path.join(pathRusOrg, key)
+        nameFile = allTextValue[allElement[0]][1]
+        print u"\nВ предпологаемом файле %s, который должен быть в папке %s, отсутствует:"%(nameFile, key)
         for element in allElement:
-            #newElement = ET.Element(element)
-            #newElement.text = allTextValue[element]
-            #root.append(newElement)
-            subElement = ET.SubElement(root, element)
-            subElement.text = allTextValue[element]
-        tree.write(path, encoding="utf-8", xml_declaration=True)        
+            print u"%s значение: %s"%(element, allTextValue[element][0])
+
+"""Эта проверка не актуальна до тех пор пока скрипт не будет определять абстрактные переменные
+for key in allTextExcess.keys():
+    print u"\nВ файле %s, не существующие переменные:"%key
+    allElement = allTextExcess[key]
+    for element in allElement:
+        print u"%s"%element"""
